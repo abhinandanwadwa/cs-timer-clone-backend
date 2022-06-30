aa89a33f046df27e268e66ddceb6d8858f52f74b
abhinandanwadwa <abhinandanwadhwa5@gmail.com>
2022-06-30 22:22:58 +0530

    initial 2

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server Running at port: http://localhost:${PORT}`);
 });
