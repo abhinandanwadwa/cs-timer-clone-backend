76c4065a1403744590573d20aa3eeb2472ce4f85
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-08-10 11:31:21 +0530

    Initial commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const mongoURI = "mongodb://localhost:27017/timer";
+const mongoURI = "mongodb+srv://abhinandan__wadhwa:asWQn4NJjqldwjwI@cluster0.rt1oy.mongodb.net/?retryWrites=true&w=majority/timer";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
